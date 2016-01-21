3a5b6860d1568e4ae34483caddd67d1edae8cd99
Rafael <r4fael@live.com>
2016-01-20 22:29:37 -0300

    Atualização 20/01
    teste entrada/saida

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,7 +1,7 @@
 function runScript(e) {
   if (e.keyCode == 13) {
     adacionarSaida(pegaValorInput());
-    limpaImput();
+    limpaInput();
   }
 }
 
@@ -9,14 +9,30 @@ function pegaValorInput() {
   return document.getElementById("scriptBox").value;
 }
 
-function limpaImput() {
+function limpaInput() {
   document.getElementById("scriptBox").value = "";
 }
 
 function adacionarSaida(string) {
   output_console.push(string);
   output_console.shift();
-  document.getElementById("output").value = output_console.join("\n");
+  atualizarConsole();
+  //document.getElementById("output").value = output_console.join("\n");
+}
+
+function limpaConsole() {
+  document.getElementById("output").value = "";
+}
+
+function atualizarConsole(){
+  limpaConsole();
+  for (var i = 0; i < output_console.length; i++) {
+    if(output_console[i] !== undefined) {
+      //document.getElementById("output").value += (output_console[i]).concat("\n");
+      //Mostra posicao
+      document.getElementById("output").value += (output_console[i]).concat(" ===> pos: ").concat(i).concat("\n");
+    }
+  }
 }
 
 function renderInput(bool) {
